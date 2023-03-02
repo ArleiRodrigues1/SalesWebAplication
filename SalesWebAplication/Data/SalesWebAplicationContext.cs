80d99ae0b385624c9a99862e9206c40ed2513152
ArleiRodrigues1 <Arlei.freire011@gmail.com>
2023-03-02 15:14:51 -0300

    Crud Scaffolding

---
+++ SalesWebAplication/Data/SalesWebAplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebAplication.Models
+{
+    public class SalesWebAplicationContext : DbContext
+    {
+        public SalesWebAplicationContext (DbContextOptions<SalesWebAplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebAplication.Models.Department> Department { get; set; }
+    }
+}
